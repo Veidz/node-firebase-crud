8a5e554a802993de4da04852f519965a8c1d5328
Veidz <joaovictorveidz@gmail.com>
2022-06-15 00:37:44 -0300

    feat: create user route

---
+++ routes/userRoutes.js
@@ -0,0 +1,10 @@
+import express from 'express';
+import { createUser } from '../controllers/userController.js';
+
+const router = express.Router();
+
+router.post('/user', createUser);
+
+export default {
+  routes: router
+};
